cb9194ec923d189b087709f7a091c069f07b1291
Howard Zhang <howard.zhang@arm.com>
2019-07-17 02:00:55 -0700

    add user
    Signed-off-by: Howard Zhang <howard.zhang@arm.com>

--- code/namespace_test.c
+++ code/namespace_test.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/capability.h>
 #include <stdio.h>
 #include <sched.h>
 #include <signal.h>
@@ -16,6 +17,10 @@ char* const child_args[] = {
 
 int child_main(void* args){
 	printf("in child process\n");
+	printf("eUID = %ld; eGID = %ld; ", (long) geteuid(), (long) getegid());
+	cap_t caps;
+	caps = cap_get_proc();
+	printf("capability: %s\n", cap_to_text(caps, NULL));
 	sethostname("NewNameSpace", 12);
 	execv(child_args[0],child_args);
 	return 1;
@@ -23,7 +28,7 @@ int child_main(void* args){
 
 int main(){
 	printf("start main function\n");
-	int child_pid = clone(child_main, child_stack + STACK_SIZE,CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWUTS | SIGCHLD, NULL);
+	int child_pid = clone(child_main, child_stack + STACK_SIZE, CLONE_NEWUSER | CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWUTS | SIGCHLD, NULL);
 	waitpid(child_pid, NULL, 0);
 	printf("exit\n");
 	return 0;
