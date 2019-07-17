7ebb692a67e5335e4e3a781404fe23c0f3635ce4
Howard Zhang <howard.zhang@arm.com>
2019-07-16 20:10:04 -0700

    uts test
    Signed-off-by: Howard Zhang <howard.zhang@arm.com>

---
+++ code/namespace_test.c
@@ -0,0 +1,30 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+static char child_stack[STACK_SIZE];
+char* const child_args[] = {
+	"/bin/bash",
+	NULL
+};
+
+int child_main(void* args){
+	printf("in child process\n");
+	sethostname("NewNameSpace", 12);
+	execv(child_args[0],child_args);
+	return 1;
+}
+
+int main(){
+	printf("start main function\n");
+	int child_pid = clone(child_main, child_stack + STACK_SIZE, CLONE_NEWUTS | SIGCHLD, NULL);
+	waitpid(child_pid, NULL, 0);
+	printf("exit\n");
+	return 0;
+}
